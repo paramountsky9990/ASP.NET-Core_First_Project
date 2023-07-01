e1e44395e7161785dd05df0c65be5a53de3f26fd
Raymond <137667254+paramountsky9990@users.noreply.github.com>
2023-07-01 22:33:03 +0900

    Middle commit

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        /* public IActionResult Index()
+         {
+             return View();
+         }*/
+
+        // 
+        // GET: /HelloWorld/
+
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            //return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+
+        //public string Welcome(string name, int numTimes = 1, int ID = 1)
+        //{
+        //    //return "This is the Welcome action method...";
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}, ID: {ID}");
+        //}
+    }
+}
---
+++ MvcMovie/MvcMovie/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class TestController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my test action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
