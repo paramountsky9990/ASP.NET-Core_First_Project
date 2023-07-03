3394acf24003fe1422db73f2d11ad7e5e9cdb0d9
Raymond <137667254+paramountsky9990@users.noreply.github.com>
2023-07-03 09:01:49 +0900

    View add

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -5,10 +5,10 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        /* public IActionResult Index()
-         {
-             return View();
-         }*/
+        public IActionResult Index()
+        {
+            return View();
+        }
 
         // 
         // GET: /HelloWorld/
@@ -17,24 +17,28 @@ namespace MvcMovie.Controllers
         //{
         //    return "This is my default action...";
         //}
-        public IActionResult Index()
-        {
-            return View();
-        }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int numTimes = 1)
-        {
-            //return "This is the Welcome action method...";
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
-        }
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    //return "This is the Welcome action method...";
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
 
         //public string Welcome(string name, int numTimes = 1, int ID = 1)
         //{
         //    //return "This is the Welcome action method...";
         //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}, ID: {ID}");
         //}
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
     }
 }
---
+++ MvcMovie/MvcMovie/Models/Moviecs.cs
@@ -0,0 +1,6 @@
+﻿namespace MvcMovie.Models
+{
+    public class Moviecs
+    {
+    }
+}
