d6078e79f0632440e07118e522c8bfb12cfd1af4
Raymond <137667254+paramountsky9990@users.noreply.github.com>
2023-07-12 04:37:03 +0900

    middle

--- MvcMovie/MvcMovie/Program.cs
+++ MvcMovie/MvcMovie/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MvcMovieContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
